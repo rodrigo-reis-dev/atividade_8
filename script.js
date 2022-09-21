c636c3c103321292aadbbbcb229294d6649b1e9a
Rodrigo Reis <rodrigoreiscs@gmail.com>
2022-09-21 14:33:29 +0100

    JS added, project done

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function calcIMC() {
+  let peso = document.querySelector('.pe').value;
+  let altura = document.querySelector('.al').value;
+  let resultado = peso / (altura * altura);
+  document.querySelector('.resultado').innerHTML = Math.round(resultado);
+}
 
+document.querySelector('.btn').addEventListener('click', calcIMC);
